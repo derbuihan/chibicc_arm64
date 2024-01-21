6bfe1baa85143aac6dd9e6b4eda9bcf1ce6fabf2
derbuihan <derbuihan@gmail.com>
2024-01-21 21:56:59 +0900

    replace remaining identifiers with 0 in macro constexpr

--- preprocess.c
+++ preprocess.c
@@ -260,6 +260,14 @@ static long eval_const_expr(Token **rest, Token *tok) {
     error_tok(start, "no expression");
   }
 
+  for (Token *t = expr; t->kind != TK_EOF; t = t->next) {
+    if (t->kind == TK_IDENT) {
+      Token *next = t->next;
+      *t = *new_num_token(0, t);
+      t->next = next;
+    }
+  }
+
   Token *rest2;
   long val = const_expr(&rest2, expr);
 
--- test/macro.c
+++ test/macro.c
@@ -292,5 +292,12 @@ int main() {
 #endif
   ASSERT(4, m);
 
+#if no_such_symbol == 0
+  m = 5;
+#else
+  m = 6;
+#endif
+  ASSERT(5, m);
+
   return 0;
 }
